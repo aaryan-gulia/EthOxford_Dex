3af6a84b836b727742ea0b18e4c5602608c6da6f
Sparkton <akdubey9925@gmail.com>
2024-03-10 16:48:29 +0000

    contract update

--- Demo/SampleSwap.sol
+++ Demo/SampleSwap.sol
@@ -1,3 +1,8 @@
+// (c) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
 pragma solidity ^0.8.0;
 
 contract TokenTransfer {
@@ -13,18 +18,23 @@ contract TokenTransfer {
   // Event to signal token transfer
   event Transferred(address indexed to, uint256 amount);
 
+  uint256 initial_value = 100;
+
   constructor() public {
     // Initialize accounts with 0 tokens (optional, adjust as needed)
-    accounts[msg.sender] = Account(0, 0);
+    accounts[msg.sender] = Account(100, 100);
   }
 
   // Function to transfer tokens
   function transfer(uint256 amount) public {
-    // Ensure sufficient balance in tokenA
-    require(accounts[msg.sender].tokenA >= amount, "Insufficient tokenA balance");
 
+
+    // Ensure sufficient balance in tokenA
+    //require(accounts[msg.sender].tokenA >= amount, "Insufficient tokenA balance");
+    accounts[msg.sender].tokenB = initial_value;
+    accounts[msg.sender].tokenA = initial_value;
     // Update account balances
-    accounts[msg.sender].tokenA -= amount;
+    //accounts[msg.sender].tokenA -= amount;
     accounts[msg.sender].tokenB += amount;
 
     // Emit transfer event
--- Demo/receiverOnDispatch.sol
+++ Demo/receiverOnDispatch.sol
@@ -7,10 +7,12 @@ pragma solidity 0.8.18;
 
 import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterMessenger.sol";
 import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterReceiver.sol";
+import "./SampleSwap.sol";
 
 contract ReceiverOnDispatch is ITeleporterReceiver {
 
     ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+    TokenTransfer public swapContract = TokenTransfer(0x73007B30dE8A22F27019428f8B5D989058C9C85b);
   
     uint public lastMessage;
 
@@ -30,7 +32,7 @@ contract ReceiverOnDispatch is ITeleporterReceiver {
         address destinationAddress,
         string calldata message
     ) external {
-
+        swapContract.transfer(lastMessage); // Assuming 'transfer' is the relevant function 
         teleporterMessenger.sendCrossChainMessage(
             TeleporterMessageInput({
                 destinationBlockchainID: 0x9f3be606497285d0ffbb5ac9ba24aa60346a9b1812479ed66cb329f394a4b1c7,
